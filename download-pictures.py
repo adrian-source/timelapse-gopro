1b76a79bcdc2674a0f3ae002a7f78e1a1addcdc5
adrian-source <adrian.sitterle@gmail.com>
2014-06-26 23:17:53 -0600

    Create download-pictures.py

---
+++ download-pictures.py
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# replace the numbers in bracktets to define the rango of photos to download
+
+#python timelapse-gopro.py wake
+
+curl -# -O  '10.5.5.9:8080/videos/DCIM/101GOPRO/GOPR[0058-0085].JPG'
+
+#python timelapse-gopro.py sleep
